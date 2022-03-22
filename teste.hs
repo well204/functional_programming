eec7c789fbd18bde7ed67b43c9165a1f42dc88a9
well <well.santos@alu.ufc.br>
2022-03-22 16:01:11 -0300

    second commit

---
+++ teste.hs
@@ -0,0 +1,21 @@
+-- um comentário em haskell
+{- aaaa
+comentário multilinha
+-}
+-- :l nome_do_arquivo.hs (rodando o código)
+ 
+--a :: Double 
+--a = 1 + 11 * 3 / 4
+--b = div 9 2
+
+--c = True 
+--d = False
+ x = "this is a string so big" ++ " muito mesmo"
+ name = ['W', 'e', 'l', 'l']
+ f = name !! 2 -- mostra a posição que você quer no vetor de char
+
+ g = [1, 2, 3, 4, 5, 6, 7]
+ i = [1..25]
+ alfabeto = ['a'..'z']
+ j = [3, 6.. 300]
+ aa = [1, 9..]
